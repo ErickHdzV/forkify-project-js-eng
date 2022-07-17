7aa49ff1f19ca2121be8e3fab330b404b27c9f92
ErickHdzV <erick.hdzv12@gmail.com>
2022-07-17 13:41:02 -0500

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'bae8c22f-df1d-4e6b-ad36-9a33cd6202eb';
+export const MODAL_CLOSE_SEC = 2.5;
